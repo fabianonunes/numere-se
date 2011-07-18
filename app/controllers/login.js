5a4932a5cd9d3a11234123ba0663fee81ca7787d
Fabiano Nunes <fabiano@nunes.me>
2011-07-17 22:22:14 -0300

    decoupling login logic from req/res

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,8 +1,27 @@
 
 var secure = app.settings.secure
-	, User = app.settings.models.user;
+	, User = app.settings.models.user
+	, qs		= require('querystring');
 
 exports.create = function(req, res, next){
+
+	secure.login_d(req.body.user, req.body.pass).then(function(cookie){
+
+		res.cookie('v', qs.stringify(cookie), {
+			expires: new Date(cookie.x)
+			, httpOnly: true
+			// , secure: true
+		});
+
+		res.send({ ok : true });
+
+	}, function(){
+		res.send(403);
+	});
+
+}
+
+exports.create_ = function(req, res, next){
 
 	User.findOne({username : req.body.user}, function(err, user){
 
